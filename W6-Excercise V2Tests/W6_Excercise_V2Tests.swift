dd0277088e42620a84566c635f88c9d0b99c22a1
Bryan <bchristoper@student.ciputra.ac.id>
2024-10-25 12:04:38 +0700

    Initial Commit

---
+++ W6-Excercise V2Tests/W6_Excercise_V2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  W6_Excercise_V2Tests.swift
+//  W6-Excercise V2Tests
+//
+//  Created by student on 25/10/24.
+//
+
+import Testing
+@testable import W6_Excercise_V2
+
+struct W6_Excercise_V2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ W6-Excercise V2UITests/W6_Excercise_V2UITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  W6_Excercise_V2UITestsLaunchTests.swift
+//  W6-Excercise V2UITests
+//
+//  Created by student on 25/10/24.
+//
+
+import XCTest
+
+final class W6_Excercise_V2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
